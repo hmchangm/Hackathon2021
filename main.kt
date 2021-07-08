ee26f2c817f0dd535cdede636e1f66fe287a662b
hmchangm <dickgg@gmail.com>
2021-07-08 08:04:27 +0800

    1. remove type declaration 2. change some var to val

--- main.kt
+++ main.kt
@@ -1,40 +1,38 @@
-fun main(args: Array<String>) {
-    var mentors: List<String> = listOf("079399", "096964", "063859", "083257", "096596", "116819", "109491", "062694")
-    var descSortedNtSumMap: Map<String, Int> = getDescSortedNtSumMap(mentors)
+fun main() {
+    val mentors = listOf("079399", "096964", "063859", "083257", "096596", "116819", "109491", "062694")
+    val groups = getShuffledGroups(mentors.size)
+    val descSortedNtSumMap = toDescSortedNtSumMap(mentors)
     println("Sort mentors by NT sum = $descSortedNtSumMap")
-    var groups: MutableList<String> = getShuffledGroups(mentors.size)
     println("Generate the shuffled groups = $groups")
-    var mentorsGroup: MutableMap<String, String> = getMentorGroupMap(descSortedNtSumMap, groups)
+    val mentorsGroup = assignMentorGroupMap(descSortedNtSumMap, groups)
     println("Final groups = $mentorsGroup")
 }
 
-fun getMentorGroupMap(sortedNts: Map<String, Int>, shuffledGroup: MutableList<String>): MutableMap<String, String> {
-    var mentorsGroup: MutableMap<String, String> = mutableMapOf()
-    var indexI:Int = 0
-    sortedNts.forEach() { it->
-        mentorsGroup.putIfAbsent(it.key, shuffledGroup[indexI++])
+fun assignMentorGroupMap(sortedNts: Map<String, Int>, shuffledGroup: MutableList<String>): MutableMap<String, String> {
+    var mentorsGroup = mutableMapOf<String, String>()
+    for ((i, v) in sortedNts.entries.withIndex()) {
+        mentorsGroup[v.key]=shuffledGroup[i]
     }
     return mentorsGroup
 }
 
-fun getDescSortedNtSumMap(nts: List<String>): Map<String, Int> {
-    var mentorsNtSumMap: MutableMap<String, Int> = mutableMapOf()
-    nts.forEach { it ->
+fun toDescSortedNtSumMap(nts: List<String>): Map<String, Int> {
+    var mentorsNtSumMap = mutableMapOf<String, Int>()
+    nts.forEach {
         var charToIntSum: Int = 0
-        it.toList().forEach { charIt ->
-            charToIntSum += charIt.toString().toInt()
+        it.forEach { char ->
+            charToIntSum += char.toInt()
         }
-        mentorsNtSumMap.putIfAbsent(it, charToIntSum)
+        mentorsNtSumMap[it] = charToIntSum
     }
     return mentorsNtSumMap.toList().sortedByDescending { (_, value) -> value }.toMap()
 }
 
 fun getShuffledGroups(groupsCnt: Int): MutableList<String> {
-    var groups: MutableList<String> = mutableListOf()
+    var groups = mutableListOf<String>()
     for (i in 1..groupsCnt) {
-        groups.add((i+65-1).toChar().toString())
+        groups.add((i + 65 - 1).toChar().toString())
     }
     groups.shuffle()
     return groups
 }
-
